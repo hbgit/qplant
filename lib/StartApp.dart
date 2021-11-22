6ebd681036ce7238de024f78c9987ab848758ae8
Herbert Rocha <herberthb12@gmail.com>
2021-11-22 19:55:27 -0400

    Add responsive support using package
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- lib/StartApp.dart
+++ lib/StartApp.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:qplant/controller/RouteGenerator.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class StartApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, widget) => ResponsiveWrapper.builder(
+          BouncingScrollWrapper.builder(context, widget!),
+          defaultName: MOBILE,
+          minWidth: 480,
+          defaultScale: true,
+          breakpoints: [
+            ResponsiveBreakpoint.resize(480, name: MOBILE),
+            ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+            ResponsiveBreakpoint.autoScale(1700, name: 'XL'),
+            ResponsiveBreakpoint.autoScale(2460, name: '4K'),
+          ]),
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
