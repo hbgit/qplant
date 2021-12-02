bf02388a6504cde7ca3d4bb8f7b4e94af417eca7
Herbert Rocha <herberthb12@gmail.com>
2021-12-02 18:42:59 -0400

    Add google maps API
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- lib/view/ClassifyResult.dart
+++ lib/view/ClassifyResult.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ClassifyResult extends StatefulWidget {
   final Uint8List imageInBytes;
@@ -14,9 +17,34 @@ class ClassifyResult extends StatefulWidget {
 }
 
 class _ClassifyResultViewState extends State<ClassifyResult> {
-  int _precisionValue = 6;
+  int _precisionValue = 3;
   int _likeNumber = 10;
   final ScrollController _firstController = ScrollController();
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  final Set<Marker> _markers = {};
+
+  _markersMaps() {
+    _markers.add(
+      Marker(
+          markerId: MarkerId("AM"),
+          position: const LatLng(-4.82386429861951, -63.96572147309308),
+          icon: BitmapDescriptor.defaultMarker),
+    );
+    _markers.add(
+      Marker(
+          markerId: MarkerId("PA"),
+          position: const LatLng(-5.611595873037647, -52.45205026292503),
+          icon: BitmapDescriptor.defaultMarker),
+    );
+  }
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(1.5862395405133518, -60.977440395644884),
+    zoom:
+        4, // https://developers.google.com/maps/documentation/android-sdk/views
+  );
 
   _precisionBarValue() {
     if (_precisionValue > 7) {
@@ -48,107 +76,172 @@ class _ClassifyResultViewState extends State<ClassifyResult> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.all(5),
-        child: Container(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                //crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      Container(
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: Image.memory(
-                              widget.imageInBytes,
-                              height: 150,
-                              //width: 200,
-                            )),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    width: 30,
-                  ),
-                  Flexible(
+    _markersMaps();
+    return SingleChildScrollView(
+      child: Padding(
+          padding: EdgeInsets.all(5),
+          child: Container(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  //crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.memory(
+                                widget.imageInBytes,
+                                height: 150,
+                                //width: 200,
+                              )),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Flexible(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      //crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "RESULTADO",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            "Precisão:",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        _precisionBarValue(),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Número de curtidas: $_likeNumber",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            FaIcon(
+                              FontAwesomeIcons.thumbsUp,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            FaIcon(
+                              FontAwesomeIcons.bookmark,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            FaIcon(
+                              FontAwesomeIcons.exclamationCircle,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ],
+                        )
+                      ],
+                    )),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                    padding: EdgeInsets.all(7),
+                    child: Card(
                       child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    //crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "RESULTADO",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          "Precisão:",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      _precisionBarValue(),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Número de curtidas: $_likeNumber",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          FaIcon(
-                            FontAwesomeIcons.thumbsUp,
-                            color: Colors.white,
-                            size: 30,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              FaIcon(
+                                FontAwesomeIcons.search,
+                                color: Colors.green,
+                                size: 30,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                "Descrição:",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    color: Colors.green,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18),
+                              ),
+                            ],
                           ),
-                          FaIcon(
-                            FontAwesomeIcons.bookmark,
-                            color: Colors.white,
-                            size: 30,
+                          SizedBox(
+                            height: 10,
                           ),
-                          FaIcon(
-                            FontAwesomeIcons.exclamationCircle,
-                            color: Colors.white,
-                            size: 30,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: SizedBox(
+                                  height: 200,
+                                  child: Center(
+                                    child: Padding(
+                                      padding: EdgeInsets.all(5),
+                                      child: ListView.builder(
+                                          controller: _firstController,
+                                          itemCount: 100,
+                                          itemBuilder: (BuildContext context,
+                                              int index) {
+                                            return Padding(
+                                              padding:
+                                                  const EdgeInsets.all(8.0),
+                                              child: Text(
+                                                  'Scrollable 1 : >>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>> Index $index'),
+                                            );
+                                          }),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ],
-                      )
-                    ],
-                  )),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
+                      ),
+                    )),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
                   padding: EdgeInsets.all(7),
                   child: Card(
                     child: Column(
@@ -158,7 +251,7 @@ class _ClassifyResultViewState extends State<ClassifyResult> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             FaIcon(
-                              FontAwesomeIcons.search,
+                              FontAwesomeIcons.mapMarker,
                               color: Colors.green,
                               size: 30,
                             ),
@@ -166,7 +259,7 @@ class _ClassifyResultViewState extends State<ClassifyResult> {
                               width: 10,
                             ),
                             Text(
-                              "Descrição:",
+                              "Encontrada em:",
                               textAlign: TextAlign.left,
                               style: TextStyle(
                                   color: Colors.green,
@@ -175,30 +268,21 @@ class _ClassifyResultViewState extends State<ClassifyResult> {
                             ),
                           ],
                         ),
-                        SizedBox(
-                          height: 10,
-                        ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Expanded(
                               child: SizedBox(
-                                height: 200,
-                                child: Center(
-                                  child: Padding(
-                                    padding: EdgeInsets.all(5),
-                                    child: ListView.builder(
-                                        controller: _firstController,
-                                        itemCount: 100,
-                                        itemBuilder:
-                                            (BuildContext context, int index) {
-                                          return Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Text(
-                                                'Scrollable 1 : >>>>>>>>>>>>>>>>>>>>>>>>>>>Index $index'),
-                                          );
-                                        }),
-                                  ),
+                                height: 500,
+                                width: MediaQuery.of(context).size.width,
+                                child: GoogleMap(
+                                  mapType: MapType.normal,
+                                  markers: _markers,
+                                  initialCameraPosition: _kGooglePlex,
+                                  onMapCreated:
+                                      (GoogleMapController controller) {
+                                    _controller.complete(controller);
+                                  },
                                 ),
                               ),
                             ),
@@ -206,10 +290,11 @@ class _ClassifyResultViewState extends State<ClassifyResult> {
                         ),
                       ],
                     ),
-                  )),
-              Row(),
-            ],
-          ),
-        ));
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
