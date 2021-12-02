0948aeefb0849c7e6739621b842122b24971a2df
Herbert Rocha <herberthb12@gmail.com>
2021-12-01 20:03:18 -0400

    Update result screen
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

--- lib/view/ClassifyResult.dart
+++ lib/view/ClassifyResult.dart
@@ -1,6 +1,8 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class ClassifyResult extends StatefulWidget {
   final Uint8List imageInBytes;
@@ -12,6 +14,38 @@ class ClassifyResult extends StatefulWidget {
 }
 
 class _ClassifyResultViewState extends State<ClassifyResult> {
+  int _precisionValue = 6;
+  int _likeNumber = 10;
+  final ScrollController _firstController = ScrollController();
+
+  _precisionBarValue() {
+    if (_precisionValue > 7) {
+      return StepProgressIndicator(
+        totalSteps: 10,
+        currentStep: 10,
+        size: 20,
+        selectedColor: Colors.green,
+        unselectedColor: Colors.white,
+      );
+    } else if (_precisionValue > 5 && _precisionValue <= 7) {
+      return StepProgressIndicator(
+        totalSteps: 10,
+        currentStep: _precisionValue,
+        size: 20,
+        selectedColor: Colors.yellow,
+        unselectedColor: Colors.white,
+      );
+    } else {
+      return StepProgressIndicator(
+        totalSteps: 10,
+        currentStep: _precisionValue,
+        size: 20,
+        selectedColor: Colors.red,
+        unselectedColor: Colors.white,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -21,7 +55,7 @@ class _ClassifyResultViewState extends State<ClassifyResult> {
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                //crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Column(
                     children: [
@@ -30,14 +64,19 @@ class _ClassifyResultViewState extends State<ClassifyResult> {
                             borderRadius: BorderRadius.circular(20),
                             child: Image.memory(
                               widget.imageInBytes,
-                              height: 200,
-                              width: 200,
+                              height: 150,
+                              //width: 200,
                             )),
                       ),
                     ],
                   ),
-                  Column(
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Flexible(
+                      child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Text(
                         "RESULTADO",
@@ -45,21 +84,129 @@ class _ClassifyResultViewState extends State<ClassifyResult> {
                         style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
-                            fontSize: 18),
+                            fontSize: 24),
                       ),
-                      Text(
-                        "100%",
-                        textAlign: TextAlign.right,
-                        style: TextStyle(
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          "Precisão:",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      _precisionBarValue(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Número de curtidas: $_likeNumber",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.thumbsUp,
                             color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12),
-                      ),
+                            size: 30,
+                          ),
+                          FaIcon(
+                            FontAwesomeIcons.bookmark,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                          FaIcon(
+                            FontAwesomeIcons.exclamationCircle,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        ],
+                      )
                     ],
-                  )
+                  )),
                 ],
               ),
-              Row(),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                  padding: EdgeInsets.all(7),
+                  child: Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FaIcon(
+                              FontAwesomeIcons.search,
+                              color: Colors.green,
+                              size: 30,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "Descrição:",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: SizedBox(
+                                height: 200,
+                                child: Center(
+                                  child: Padding(
+                                    padding: EdgeInsets.all(5),
+                                    child: ListView.builder(
+                                        controller: _firstController,
+                                        itemCount: 100,
+                                        itemBuilder:
+                                            (BuildContext context, int index) {
+                                          return Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                                'Scrollable 1 : >>>>>>>>>>>>>>>>>>>>>>>>>>>Index $index'),
+                                          );
+                                        }),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )),
               Row(),
             ],
           ),
